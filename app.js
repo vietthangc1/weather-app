e325db2403a678ae56c2cdbf5ee5caeb73b9f957
vietthangc1 <65752240+vietthangc1@users.noreply.github.com>
2021-10-01 11:17:07 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+let city = "London";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+	let url =
+		"https://api.openweathermap.org/data/2.5/weather?q=" +
+		city +
+		"&appid=c2768ef5adf976d26c0fcf8b8c9cb5f8&units=metric";
+	https.get(url, function (request) {
+		request.on("data", function (data) {
+			let info = JSON.parse(data);
+			let tempt = info.main.temp;
+			let icon_code = info.weather[0].icon;
+			let tempt_min = info.main.temp_min;
+			let tempt_max = info.main.temp_max;
+			let url_icon =
+				"http://openweathermap.org/img/wn/" + icon_code + "@2x.png";
+			let options = {
+        th_city: city,
+				th_imgsrc: url_icon,
+				th_tempt_min: tempt_min,
+				th_tempt_max: tempt_max,
+				th_tempt: tempt,
+			};
+			res.render("index", options);
+		});
+	});
+});
+
+app.post("/", function (req, res) {
+	city = req.body.th_city;
+	console.log(city);
+	res.redirect("/");
+});
+
+app.listen(3000, function () {
+	console.log("Server run!");
+});
